97a05fa716c78793c17bd77b610337cdebd50ec2
sua00 <88168765+sua00@users.noreply.github.com>
2024-08-24 16:36:06 +0900

    Create 240824_2739.cpp

---
+++ 240824_2739.cpp
@@ -0,0 +1,17 @@
+//반복문_구구단 출력
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N,i;
+    cin>>N;
+    
+
+        for(int a=1;a<10;a++){
+            cout<< N<<" * "<<a<<" = "<<N*a<<endl;
+        }
+    
+
+    return 0;
+}

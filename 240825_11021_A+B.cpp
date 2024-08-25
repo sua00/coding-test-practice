8b07cbaac47abc212367d8377386155d63a0c861
sua00 <88168765+sua00@users.noreply.github.com>
2024-08-25 18:05:51 +0900

    Update 240825_11021_A+B.cpp

--- 240825_11021_A+B.cpp
+++ 240825_11021_A+B.cpp
@@ -15,3 +15,21 @@ int main()
 
     return 0;
 }
+
+//11022번 문제 : 또 다른 서식 (Case #n: a+b = N)
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int T; // 반복횟수
+    int* a = new int[T]; //a 값 입력
+    int* b= new int[T]; //b 값 입력
+    cin>>T;
+    for(int i=1 ; i<=T;i++){
+        cin>> a[i]>>b[i];
+        cout<<"Case #"<<i<<": "<<a[i]<<" + "<< b[i]<<" = " <<a[i]+b[i]<<endl; //Case #n: X
+    }
+
+    return 0;
+}
